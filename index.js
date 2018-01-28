d7200167eae9f13e6926b428ee28f335bcac4131
data-doge <eugene@enspiral.com>
2018-01-27 21:22:27 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,57 @@
+const $ = require('jquery')
+const raf = require('raf')
+const range = require('lodash.range')
+const { sin, PI } = Math
+
+const canvas = $('#floor')[0]
+const ctx = canvas.getContext('2d')
+
+const canvasWidth = 400
+const canvasHeight = 400
+
+const drawCurve = ({ x0, y0, length, theta, thetaOffset }) => {
+  let amp = 100 * sin(theta * 0.021 * 2 * PI)
+  let dx = amp * sin(theta * 2 * PI + thetaOffset)
+  let cy = 50 + 20 * sin(theta * 0.29 * 2 * PI + PI) // 30 to 70
+  let y1 = y0 + length
+  ctx.beginPath()
+  ctx.moveTo(x0, y0)
+  ctx.bezierCurveTo(
+    x0 + dx, y0 + cy,
+    x0 - dx, y1 - cy,
+    x0, y1
+  )
+  const gradient = ctx.createLinearGradient(x0, y0, x0, y1)
+  gradient.addColorStop('0', '#FFABBE')
+  gradient.addColorStop('0.5', '#FFFFFF')
+  gradient.addColorStop('1', '#FFABBE')
+  ctx.strokeStyle = gradient
+  ctx.lineWidth = 3
+  ctx.stroke()
+}
+
+const clear = () => {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+}
+
+let theta = 0
+
+const draw = () => {
+  clear()
+  theta = theta + 0.01
+
+  range(-100, 500, 100).forEach(y => {
+    range(-50, 460, 10).forEach(x => {
+      drawCurve({
+        x0: x,
+        y0: y,
+        length: 100,
+        theta,
+        thetaOffset: 0
+      })
+    })
+  })
+  raf(draw)
+}
+
+raf(draw)
